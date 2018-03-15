d49d968a91ef1694a4dafbeda4cb9199ea8b0229
zhangrujie <1468196979@qq.com>
2018-03-15 17:43:53 +0800

    demo

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+var t;
+var index = 0;
+/////自动播放
+t = setInterval(play, 3000)
+
+function play() {
+  index++;
+  if (index > 2) {
+    index = 0
+  }
+  // console.log(index)
+  $(".main_carousel ul li").eq(index).css({
+    "background": "#999",
+    "border": "1px solid #ffffff"
+  }).siblings().css({
+    "background": "#cccccc",
+    "border": ""
+  })
+
+  $(".carousel_border").eq(index).fadeIn(1000).siblings().fadeOut(1000);
+};
+
+///点击鼠标 图片切换
+$(".main_carousel ul li").click(function() {
+  // console.log('wewrd');
+  //添加 移除样式
+  //$(this).addClass("lito").siblings().removeClass("lito"); //给当前鼠标移动到的li增加样式 且其余兄弟元素移除样式   可以在样式中 用hover 来对li 实现
+  $(this).css({
+    "background": "#999",
+    "border": "1px solid #ffffff"
+  }).siblings().css({
+    "background": "#cccccc"
+  });
+  var index = $(this).index(); //获取索引 图片索引与按钮的索引是一一对应的
+  // console.log(index);
+
+  $(".carousel_border").eq(index).fadeIn(1000).siblings().fadeOut(1000); // siblings  找到 兄弟节点(不包括自己）
+});
